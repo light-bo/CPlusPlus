2d19291b2384fa715d22163c425d6e0847e13a1d
light_bo <light_bo@126.com>
2016-10-31 23:56:45 +0800

    modified quick_sort.cpp file

--- basic_algorithms_and_typical_problems/Sort/quick_sort.cpp
+++ basic_algorithms_and_typical_problems/Sort/quick_sort.cpp
@@ -1,4 +1,6 @@
-/* quick sort: 2 partition method*/
+/**
+ *  quick sort: 2 partition method
+ */
 inline void swap(int& x, int& y){
 	int temp = x;
 	x = y;
@@ -17,7 +19,9 @@ int partition_method1(int* array, int low, int high){
 	return high;
 }
 
-/* this method must take up a extra space(a array element)*/
+/**
+ * this method must take up a extra space(a array element)
+ */
 int partition_method2(int* array, int low, int high){
 	int x = array[high];
 	int i = low - 1;
@@ -25,17 +29,18 @@ int partition_method2(int* array, int low, int high){
 	for(int j=low;j<high;++j){
 		if(array[j]<=x){
 			++i;
-			swap(array[i], array[j]);	
+			swap(array[i], array[j]);
 		}
 	}
 	swap(array[i+1], array[high]);
 	return i + 1;
 }
 
-/*random version partition, the function retrive a random 
- *index i of the array, and then swap the element with the index i and
- *the element with the index high, the reason for it is to get the pilot
- *element randomly every time divide the array*/
+/** random version partition, the function retrive a random
+ * index i of the array, and then swap the element with the index i and
+ * the element with the index high, the reason for it is to get the pilot
+ * element randomly every time divide the array
+ */
 int random_partition(int* array, int low, int high){
 	int i = low + (rand()%(high-low+1));
 	swap(array[i], array[high]);
